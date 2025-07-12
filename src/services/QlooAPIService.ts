a5c7ffe7428f46a541a027b6c881537e83f3b353
wako1313 <raqelthayen.found@gmail.com>
2025-07-12 09:59:37 +0200

    Updated QlooAPIService.ts

--- src/services/QlooAPIService.ts
+++ src/services/QlooAPIService.ts
@@ -384,7 +384,7 @@ class QlooAPIService {
     try {
       await this.makeRequest('/v2/insights', {
         'filter.type': 'urn:entity:artist',
-        'signal.demographics.age': '36_to_55',
+        'signal.location.query': 'New York City',
         'take': '1'
       });
       return true;
