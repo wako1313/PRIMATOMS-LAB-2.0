40ba269402eb4b620e0891bcbf915616067b96b8
wako1313 <raqelthayen.found@gmail.com>
2025-07-15 18:36:47 +0200

    Updated PopulationControls.tsx

--- src/components/PopulationControls.tsx
+++ src/components/PopulationControls.tsx
@@ -579,11 +579,16 @@
                 </span>
                 <span className="text-green-400">
                   ⚡ {trendingEntities.length} tendances suivies
-                </span>
-                </div>
-              </div>
-            </div>
+                <div>
           </div>
+        </div>
+      </div>
+    </div>
+        </div>
+  );
+};
+
+export default PopulationControls;
         </div>
       </div>
     </div>
